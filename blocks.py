9d2a874931a0a925555c207195ad71224f3fc038
Vlad Chernoborodiuk <45739207+Marsianin007@users.noreply.github.com>
2021-12-12 15:40:03 +0100

    ділення на частини

--- blocks.py
+++ blocks.py
@@ -10,11 +10,6 @@
 import json
 import sys
 
-def print_fun(file_name, to_print):
-    with open(file_name, "w") as file:
-        json.dump(to_print, file)
-
-
 def index_for_middle(file, number):
     with open(file, "r") as file_js:
         file_py = file_js.read()
@@ -22,31 +17,29 @@ def index_for_middle(file, number):
 
         if len(file_py) % 2 == 0 and number % 2 == 0:
             index_1 = int((len(file_py) - number) / 2)
-            index_2 = int((len(file_py) - 2 + number) / 2)
-            return file_py[index_1: index_2 + 1]
+            index_2 = int((len(file_py) + number) / 2)
+            return file_py[index_1: index_2]
 
 
         if len(file_py) % 2 == 0 and number % 2 != 0:
-            center = int(len(file_py) / 2)
+            center = int(len(file_py) // 2)
             index_1 = int((center - number / 2))
             index_2 = int((center + number / 2))
             return file_py[index_1 : index_2]
 
         if len(file_py) % 2 != 0 and number % 2 == 0:
-            index_1 = int(((len(file_py) - 1) / 2))  #зміщуємо вліво
-            index_2 = int(((len(file_py) + 1) / 2))
-            return file_py[index_1: index_2 + 1]
+            index_1 = int(((len(file_py) - 1 - number) / 2))  #зміщуємо вліво
+            index_2 = int(((len(file_py) + number) / 2))
+            return file_py[index_1: index_2]
 
         if len(file_py) % 2 != 0 and number % 2 != 0:
-            center = int(len(file_py) / 2) - 1
-            index_1 = int(center - (number / 2))
-            index_2 = int(center + number / 2)
+            center = int(len(file_py) / 2)
+            index_1 = int(center - (number // 2) - 1)
+            index_2 = int(center + (number // 2) - 1)
             if number != 1:
                 return file_py[index_1 + 1: index_2 + 2]
             else:
-                return file_py[center + 1]
-
-
+                return file_py[center : center + 1]
 
 def divide_to_3_blocks(file_name, number):
     with open(file_name, "r") as file:
@@ -55,10 +48,16 @@ def divide_to_3_blocks(file_name, number):
         if str(number).isdigit():
             number = int(number)
 
-        if  str(number).isdigit() == False or number > len(text_file):
-            print("Неможливо поділити з такою довжиною блока або введена строка")
+        if  str(number).isdigit() == False:
+            print("Введена строка або від'ємне число")
+            SystemExit
 
+        elif int(number) > len(text_file):
+            print("Неможливо поділити з такою довжиною блока")
+            SystemExit
         else:
+            number = int(number)
+
             list_start = text_file[0:number]
             list_middle = index_for_middle(file_name, number)
             list_back = text_file[len(text_file) - number: len(text_file)]
@@ -66,15 +65,4 @@ def divide_to_3_blocks(file_name, number):
             print("Середина: " + str(list_middle))
             print("Кінець: " + list_back)
 
-
-
-
-
-
-
-
-divide_to_3_blocks("text.data", 1)
-#print_fun("text.data", "Hello World")
-
-
-
+divide_to_3_blocks("text.data", 0)
